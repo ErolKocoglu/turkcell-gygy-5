f7bb2281ccb1b99eb90ae17ff942a4e2088e41a3
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-08 20:46:13 +0300

    add product

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -2,8 +2,12 @@ package com.turkcell.spring_starter.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.turkcell.spring_starter.model.Product;
 
 @RestController 
 @RequestMapping("/api/product") // localhost:8080/api/product -> ProductController
@@ -22,4 +26,14 @@ public class ProductController {
     public String sayHello(@PathVariable String name, @PathVariable int age) {
         return "Hello " + name + " yaşınız: " + age;
     }
+
+    @PostMapping
+    public Product add(@RequestBody Product product){ // Json->Java objesine
+        // isim 1 haneden uzun mu?
+        // fiyat..
+        // DB'e kaydet..
+
+        return product;
+    }
+
 }
