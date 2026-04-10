9261519621b94b80d04a39760245edd27983fa58
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-10 19:51:52 +0300

    dto

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.turkcell.spring_starter.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.turkcell.spring_starter.dto.ProductCreatedResponse;
+import com.turkcell.spring_starter.dto.ProductForCreateDto;
 import com.turkcell.spring_starter.model.Product;
 
+// Altın kural: Veritabanı nesneleri requestte de responseda da kullanılamaz.
 @RestController 
 @RequestMapping("/api/product") 
 public class ProductController {
@@ -23,6 +28,7 @@ public class ProductController {
 
     @GetMapping()
     public List<Product> getAllProducts() {
+        // Veritabanındaki Product nesnelerini listele.
         return productList;
     }
 
@@ -33,9 +39,32 @@ public class ProductController {
         return productList.stream().filter(i->i.getId() == id).findFirst().orElse(null);
     }
 
+
+    // Request-Response Pattern =>
+    // Her istek-cevap kendine has bir modele sahip olmak zorunda!
+    // Birebir başka bir istek-cevap çiftiyle aynı içeriğe sahip olsa dahil!
     @PostMapping
-    public void createProduct(@RequestBody Product product) {
+    public ProductCreatedResponse createProduct(@RequestBody ProductForCreateDto productDto) {
+        // Veritabanına product nesnesini ekle..
+
+        // Sen dışardan ProductForCreateDto alıyosun 
+        // ama veritabanı Product ile çalışıyor
+
+        // Transfer => MANUAL MAPPING
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        product.setId(new Random().nextInt(999));
+
         productList.add(product);
+
+        // Domain Nesnesi -> Dto
+        ProductCreatedResponse response = new ProductCreatedResponse();
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setPrice(product.getPrice());
+
+        return response;
     }
     @PutMapping
     public void updateProduct(@RequestBody Product product) {
@@ -50,3 +79,6 @@ public class ProductController {
         ///.. Todo..
     }
 }
+
+// DTO => Data Transfer Object
+// Entity ile X (controller,service) arası veri transferi için oluşturulan sınıflardır.
